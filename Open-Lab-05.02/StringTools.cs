d81860b44b2f1146704aa77aad3e748cb495004d
Jaro32768 <jarko04@hotmail.sk>
2020-10-18 12:53:04 +0200

    vypracovanie

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -6,7 +6,36 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            string z = "";
+            string t = sentence;
+            for (int i = 0; i < sentence.Length - 1; i++)
+            {
+                string x = t[t.Length - 1].ToString();
+                string y = t[t.Length - 2].ToString();
+                if (x == y)
+                {
+                    z = "";
+                    if (x == "!")
+                    {
+                        for (int k = 0; k < t.Length - 1; k++)
+                        {
+                            z = z + t[k];
+                        }
+                        t = z;
+                    }
+                    else if (x == "?")
+                    {
+                        for (int k = 0; k < t.Length - 1; k++)
+                        {
+                            z = z + t[k];
+                        }
+                        t = z;
+                    }
+                    else break;
+                }
+                else break;
+            }
+            return t;
         }
     }
 }
